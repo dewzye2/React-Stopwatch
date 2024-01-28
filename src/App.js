b723c5efedd13b48e10057204caa146fa5039074
dewzye1@gmail.com <dewzye1@gmail.com>
2024-01-27 22:26:09 -0300

    stopwatch 1.0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Stopwatch from "./Stopwatch.jsx";
+
+function App() {
+  return (<>
+    <Stopwatch />
+    <br></br>
+    <Stopwatch />
+    <br></br>
+    <Stopwatch />
+  </>);
+}
+export default App;
